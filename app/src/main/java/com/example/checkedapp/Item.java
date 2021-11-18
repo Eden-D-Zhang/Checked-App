15de1775d2914084c48852a889b436e95aaf45e8
Eden Zhang <eden.d.zhang@gmail.com>
2021-11-18 14:39:16 -0500

    Added select_button.xml, item_layout.xml, ItemsAdapter.java, and Item.java.
    Implemented recyclerview to have a list of items when searched.
    
    Button for items turn green when selected.

---
+++ app/src/main/java/com/example/checkedapp/Item.java
@@ -0,0 +1,51 @@
+package com.example.checkedapp;
+
+import java.util.ArrayList;
+
+public class Item {
+
+    private String itemName;
+    private double itemPrice;
+    private double itemStars;
+    private boolean isInStock;
+    private String itemLink;
+
+    public Item (String name, double price, double stars, boolean stock, String link) {
+        itemName = name;
+        itemPrice = price;
+        itemStars = stars;
+        isInStock = stock;
+        itemLink = link;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public double getItemStars() {
+        return itemStars;
+    }
+
+    public boolean getItemStock() {
+        return isInStock;
+    }
+
+    public String getItemLink() {
+        return itemLink;
+    }
+
+    public static ArrayList<Item> createItemList(int numItems) {
+        ArrayList<Item> items = new ArrayList<Item>();
+
+        for (int i = 1; i <= numItems; i++) {
+            items.add(new Item("PS5", 623.00, 4.5, true, "https://empty_link"));
+        }
+
+        return items;
+    }
+
+}
